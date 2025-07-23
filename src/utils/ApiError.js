4a47e9e27d793eb2eb2543f1953c2e1d8a3e8678
aashikantkumar <aashikant.kumar2023@iem.edu.in>
2025-07-24 02:57:24 +0530

    feat: Implement user authentication with JWT and enhance error handling

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,24 +1,23 @@
-class ApiError extends Error{
+class ApiError extends Error {
     constructor(
         statusCode,
-        message="Somethin went worng",
-        errors=[],
-        statck=""
-    ){
+        message = "Something went wrong",  // Fixed typo in "Something"
+        errors = [],
+        stack = ""    // Fixed typo in parameter name
+    ) {
         super(message)
-        this.statusCode=statusCode
-        this.data=null
-        this.message=message
-        this.success=false;
-        this.errors=errors
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = errors
 
-        if(stack){
-            this.stack =statck
-
-        }else{
-            Error.captureStackTrace(this ,this.constructo)
+        if (stack) {
+            this.stack = stack    // Fixed typo in variable name
+        } else {
+            Error.captureStackTrace(this, this.constructor)  // Fixed typo in "constructor"
         }
-
     }
 }
-export {ApiError}
+
+export { ApiError }
