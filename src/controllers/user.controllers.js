2a14ccb88794c393c0922c35efc6a5b94f1e89f3
aashikantkumar <aashikant.kumar2023@iem.edu.in>
2025-07-27 02:49:56 +0530

    feat: Add subscription model and implement token refresh functionality

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,9 @@
-import  express from 'express';
 import {asyncHandler} from '../utils/asyncHandler.js';
 import {ApiError} from '../utils/ApiError.js';
 import {User} from '../models/user.model.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import {ApiResponse} from '../utils/ApiResponse.js';
+import jwt from "jsonwebtoken"
 
 
 const generateAccessAndrefreshTokens =async(userId)=>{
@@ -163,7 +163,129 @@ const logoutUser= asyncHandler( async(req,res)=>{
 
 })
 
-export  {registerUser,loginUser,logoutUser};
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+    const incomingTefreshToken = req.cookies.refreshAccessToken || req.body.refreshToken
+
+    if(!incomingTefreshToken){
+        throw new ApiError(401,"unauthorized request ")
+    }
+   try {
+     const decodedToken=jwt.verify(incomingTefreshToken,process.env.REFRESH_TOKEN_SECRET)
+     const user = User.findById(decodedToken?._id)
+       if(incomingTefreshToken){
+         throw new ApiError(401,"Invalid refresh token")
+     }
+ 
+     if(incomingTefreshToken !==user?.refreshToken){
+         throw new ApiError(401,"Refresh token is expired or used")
+     }
+     const options ={
+         httpOnly:true,
+         secure:true
+     }
+     const {accessToken,newrefreshToken}= await generateAccessAndrefreshTokens(user._id)
+   
+     return res
+     .status(200)
+     .cookie("aaccessTokekn",accessToken,options)
+     .cookie("refreshToken",newrefreshToken,options)
+     .json(
+         new ApiResponse(200,{accessToken,refreshAccessToken:newrefreshToken}
+             ,"Access token refreshed"
+         )
+     )
+   } catch (error) {
+    throw new ApiError(401,error?.message || "Invalid refresh token")
+    
+   }
+})
+
+const changecurrentPassword = asyncHandler( async (req,res)=>{
+    const {oldPassword,newPassword}=req.body
+
+    const user=await User.findById(req.user?.id)
+    const isPasswordCorrect= await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Invalid old Password")
+    }
+
+    user.password =  newPassword 
+    await user.save({validateBeforeSave:false})
+
+    return res.status(200)
+    .json( new ApiResponse(200,{},"Password changed successfully"))
+
+
+})
+
+
+const getCuurenUser=asyncHandler(async (req,res)=>{
+    return res
+    .status(200)
+    .json(200,req.user,"current user fetched successfully")
+})
+
+const updateAccoundDetails= asyncHandler(async(req,res)=>{
+    const {fullName,email} =req.body
+
+    if(!fullName || !email){
+
+        throw new ApiError(400, "All fields are required")
+
+    }
+
+    User.findByIdAndUpdate(req.user?._id,
+        {
+            $set:{
+                fullName:fullName,
+                email:email,
+
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user,"Account details updated successfully"))
+})
+
+
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+    const avatorLocalPath=req.file?.path
+
+    if(!avatorLocalPath){
+        throw new ApiError(400,"Aavatar file is missing ")
+
+    }
+
+    const avatar= await uploadOnCloudinary (avatorLocalPath)
+
+    if(!avatar.url){
+        throw new  ApiError(400,"Error while uploading on avatar ")
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },{
+            new:true
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user," avatar image updated  successfully ")
+    )
+
+})
+
+export  {registerUser,loginUser,logoutUser,refreshAccessToken,changecurrentPassword,getCuurenUser,updateAccoundDetails,updateUserAvatar};
 
 
 //get user details from frontend 
